ab37d9e4607a8689c913a73699248d512d2f271c
Dzmitry Tarasau <36638017+geek10010@users.noreply.github.com>
2018-09-24 18:58:36 +0300

    refactor function makeExchange

--- src/index.js
+++ src/index.js
@@ -19,38 +19,10 @@ module.exports = function makeExchange(currency) {
     };
 
     count('H');
-    // let h = Math.floor(currency / 50);
-    // if (h > 0) {
-    //   currency = currency % 50;
-    //   result['H'] = h;
-    // }
-
     count('Q');
-    // let q = Math.floor(currency / 25);
-    // if (q > 0) {
-    //   currency = currency % 25;
-    //   result['Q'] = q;
-    // }
-
     count('D');
-    // let d = Math.floor(currency / 10);
-    // if (d > 0) {
-    //   currency = currency % 10;
-    //   result['D'] = d;
-    // }
-
     count('N');
-    // let n = Math.floor(currency / 5);
-    // if (n > 0) {
-    //   currency = currency % 5;
-    //   result['N'] = n;
-    // }
-
     count('P');
-    // let p = currency;
-    // if (p > 0) {
-    //   result['P'] = p;
-    // }
             
     return result;
 }
