8842ee62205f5cbe990adfdfb4002c9f6c06d92c
Dzmitry Tarasau <36638017+geek10010@users.noreply.github.com>
2018-09-26 13:12:19 +0300

    refactor makeExchange function

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Return an object containing the minimum number of coins needed to make change
+module.exports = function makeExchange(input) {
+  // Return an object containing the minimum number of coins needed to make change
+  let currency = input;
 
-    if (currency <= 0) return {};
-    if (currency > 10000) {
-      return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+  if (currency <= 0) return {};
+
+  if (currency > 10000) {
+    return { error: "You are rich, my friend! We don't have so much coins for exchange" };
+  }
+
+  const result = {};
+  const moneyValues = {
+    H: 50, Q: 25, D: 10, N: 5, P: 1,
+  };
+
+  const count = (value) => {
+    const num = Math.floor(currency / moneyValues[value]);
+    if (num > 0) {
+      currency %= moneyValues[value];
+      result[value] = num;
     }
+  };
 
-    let result = {};
-    const values = {'H': 50, 'Q': 25, 'D': 10, 'N': 5, 'P': 1};
+  ['H', 'Q', 'D', 'N', 'P'].forEach(value => count(value));
 
-    const count = (value) => {
-      let num = Math.floor(currency / values[value]);
-      if (num > 0) {
-        currency = currency % values[value];
-        result[value] = num;
-      }
-    };
-
-    count('H');
-    count('Q');
-    count('D');
-    count('N');
-    count('P');
-            
-    return result;
-}
+  return result;
+};
